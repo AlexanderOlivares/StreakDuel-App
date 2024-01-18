dd76e051de715fd7b47bc1e129585b948b5a6c32
alex olivares <alexander.b.olivares@gmail.com>
2024-01-17 21:35:24 -0600

    refactor: use unstable_cache for getMatchups with revalidation every hour

--- app/dataFetching/getMatchups.ts
+++ app/dataFetching/getMatchups.ts
@@ -5,6 +5,7 @@ import {
 import prisma from "@/lib/prisma";
 import { MatchupWithOdds } from "@/lib/types/interfaces";
 import moment from "moment";
+import { unstable_cache } from "next/cache";
 
 export interface GetMatchups {
   matchups: MatchupWithOdds[];
@@ -12,32 +13,36 @@ export interface GetMatchups {
   error?: string;
 }
 
-export const getMatchups = async () => {
-  const now = moment().format("YYYYMMDD");
-  const today = dayRangeLaTimezone(now);
-  const yesterday = dayRangeLaTimezone(moment(now).subtract(1, "days").format("YYYYMMDD"));
-  const nextDay = dayRangeLaTimezone(moment(now).add(1, "days").format("YYYYMMDD"));
-  const displayDates = getYesterdayTodayTomorrowDisplayDates("America/Los_Angeles");
+export const getMatchups = unstable_cache(
+  async () => {
+    const now = moment().format("YYYYMMDD");
+    const today = dayRangeLaTimezone(now);
+    const yesterday = dayRangeLaTimezone(moment(now).subtract(1, "days").format("YYYYMMDD"));
+    const nextDay = dayRangeLaTimezone(moment(now).add(1, "days").format("YYYYMMDD"));
+    const displayDates = getYesterdayTodayTomorrowDisplayDates("America/Los_Angeles");
 
-  try {
-    const matchups: MatchupWithOdds[] = await prisma.matchups.findMany({
-      where: {
-        OR: [yesterday, today, nextDay].map(dateRange => ({ strTimestamp: dateRange })),
-        used: true,
-      },
-      include: { odds: true },
-      orderBy: [
-        {
-          strTimestamp: "asc",
+    try {
+      const matchups: MatchupWithOdds[] = await prisma.matchups.findMany({
+        where: {
+          OR: [yesterday, today, nextDay].map(dateRange => ({ strTimestamp: dateRange })),
+          used: true,
         },
-        {
-          id: "asc",
-        },
-      ],
-    });
-    return { matchups, displayDates };
-  } catch (error) {
-    console.log(error);
-    return { matchups: [], displayDates, error: "error fetching matchups" };
-  }
-};
+        include: { odds: true },
+        orderBy: [
+          {
+            strTimestamp: "asc",
+          },
+          {
+            id: "asc",
+          },
+        ],
+      });
+      return { matchups, displayDates };
+    } catch (error) {
+      console.log(error);
+      return { matchups: [], displayDates, error: "error fetching matchups" };
+    }
+  },
+  ["matchups"],
+  { tags: ["matchups"], revalidate: 60 }
+);
