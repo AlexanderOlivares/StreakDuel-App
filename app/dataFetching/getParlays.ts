71d2b4b8526e54aa2b8c5f5eb4966d46faae604b
alex olivares <alexander.b.olivares@gmail.com>
2024-01-21 18:31:28 -0600

    refactor: remove unstable_cache from getParlays. Delete unused api route

--- app/dataFetching/getParlays.ts
+++ app/dataFetching/getParlays.ts
@@ -3,7 +3,6 @@ import { options } from "../api/auth/[...nextauth]/options";
 import prisma from "@/lib/prisma";
 import { IPick, ParlayWithPicksAndOdds, PickHistory } from "@/lib/types/interfaces";
 import { getActiveMatchups, getActivePicks, getPickHistory } from "./utils/parlayUtils";
-import { unstable_cache } from "next/cache";
 
 export interface GetParlays {
   parlays: ParlayWithPicksAndOdds[];
@@ -16,107 +15,103 @@ export interface GetParlays {
   error?: string;
 }
 
-export const getParlays = unstable_cache(
-  async () => {
-    const defaultState: GetParlays = {
-      parlays: [],
-      pickHistory: [],
-      dbPickHistory: [],
-      activePoints: 100, // decide on default here
-      locked: false,
-      activePicks: [],
-      dbActivePicks: [],
-    };
+export const getParlays = async () => {
+  const defaultState: GetParlays = {
+    parlays: [],
+    pickHistory: [],
+    dbPickHistory: [],
+    activePoints: 100, // decide on default here
+    locked: false,
+    activePicks: [],
+    dbActivePicks: [],
+  };
 
-    try {
-      const session = await getServerSession(options);
+  try {
+    const session = await getServerSession(options);
 
-      if (!session) {
-        console.log("no session found");
-        return defaultState;
-      }
+    if (!session) {
+      console.log("no session found");
+      return defaultState;
+    }
 
-      const user = await prisma.user.findUnique({ where: { email: session?.user?.email ?? "" } });
+    const user = await prisma.user.findUnique({ where: { email: session?.user?.email ?? "" } });
 
-      if (!user) {
-        return { ...defaultState, error: "Error fetching user account" };
-      }
+    if (!user) {
+      return { ...defaultState, error: "Error fetching user account" };
+    }
 
-      const parlays: ParlayWithPicksAndOdds[] = await prisma.parlay.findMany({
-        where: {
-          userId: user.id,
-          // TODO add time based filters to only get parlays from current weekly round
-        },
-        orderBy: {
-          createdAt: "desc",
-        },
-        include: {
-          picks: {
-            orderBy: {
-              createdAt: "desc",
-            },
-            include: {
-              odds: true,
-            },
+    const parlays: ParlayWithPicksAndOdds[] = await prisma.parlay.findMany({
+      where: {
+        userId: user.id,
+        // TODO add time based filters to only get parlays from current weekly round
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        picks: {
+          orderBy: {
+            createdAt: "desc",
           },
+          include: {
+            odds: true,
+          },
+        },
+      },
+    });
+
+    let createdParlay;
+    if (!parlays.length) {
+      // TODO update default points when handling forfeits
+      const DEFAULT_POINTS_WAGERED = 100;
+      createdParlay = await prisma.parlay.create({
+        data: {
+          userId: user.id,
+          pointsWagered: DEFAULT_POINTS_WAGERED,
         },
       });
-
-      let createdParlay;
-      if (!parlays.length) {
-        // TODO update default points when handling forfeits
-        const DEFAULT_POINTS_WAGERED = 100;
-        createdParlay = await prisma.parlay.create({
-          data: {
-            userId: user.id,
-            pointsWagered: DEFAULT_POINTS_WAGERED,
-          },
-        });
-      }
-
-      // TODO tidy up and test this logic
-      const existingParlay: ParlayWithPicksAndOdds | Record<string, never> = parlays?.[0] ?? {};
-      const allParlayPicks = parlays.flatMap(parlay => parlay.picks) ?? [];
-
-      if (!parlays.length && !createdParlay) {
-        return { ...defaultState, error: "No parlay found" };
-      }
-
-      const locked = createdParlay ? false : existingParlay?.locked;
-      const activePicksFromDb = existingParlay?.picks ?? [];
-      const parlayIsOver =
-        !locked &&
-        activePicksFromDb.length > 0 &&
-        activePicksFromDb.every(pick => ["win", "loss", "push"].includes(pick.result));
-
-      let activePoints;
-      if (createdParlay) {
-        activePoints = createdParlay?.pointsWagered;
-      } else {
-        activePoints = parlayIsOver ? existingParlay.pointsAwarded : existingParlay.pointsWagered;
-      }
-
-      const activeMatchups = await getActiveMatchups(
-        allParlayPicks.map(({ matchupId }) => matchupId)
-      );
-
-      const activePicks = parlayIsOver ? [] : getActivePicks(existingParlay, activeMatchups);
-      const pickHistory = getPickHistory(allParlayPicks, activeMatchups);
-
-      return {
-        parlays,
-        pickHistory,
-        activePoints,
-        locked,
-        activePicks,
-        dbActivePicks: activePicks,
-        dbPickHistory: pickHistory,
-      };
-    } catch (error) {
-      console.log(error);
-      return { ...defaultState, error: "Error fetching parlays" };
     }
-  },
-  ["parlays"],
-  { tags: ["parlays"], revalidate: 10 }
-);
+
+    // TODO tidy up and test this logic
+    const existingParlay: ParlayWithPicksAndOdds | Record<string, never> = parlays?.[0] ?? {};
+    const allParlayPicks = parlays.flatMap(parlay => parlay.picks) ?? [];
+
+    if (!parlays.length && !createdParlay) {
+      return { ...defaultState, error: "No parlay found" };
+    }
+
+    const locked = createdParlay ? false : existingParlay?.locked;
+    const activePicksFromDb = existingParlay?.picks ?? [];
+    const parlayIsOver =
+      !locked &&
+      activePicksFromDb.length > 0 &&
+      activePicksFromDb.every(pick => ["win", "loss", "push"].includes(pick.result));
+
+    let activePoints;
+    if (createdParlay) {
+      activePoints = createdParlay?.pointsWagered;
+    } else {
+      activePoints = parlayIsOver ? existingParlay.pointsAwarded : existingParlay.pointsWagered;
+    }
+
+    const activeMatchups = await getActiveMatchups(
+      allParlayPicks.map(({ matchupId }) => matchupId)
+    );
+
+    const activePicks = parlayIsOver ? [] : getActivePicks(existingParlay, activeMatchups);
+    const pickHistory = getPickHistory(allParlayPicks, activeMatchups);
+
+    return {
+      parlays,
+      pickHistory,
+      activePoints,
+      locked,
+      activePicks,
+      dbActivePicks: activePicks,
+      dbPickHistory: pickHistory,
+    };
+  } catch (error) {
+    console.log(error);
+    return { ...defaultState, error: "Error fetching parlays" };
+  }
+};
